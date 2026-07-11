8ad274d26b89f84499858dccec460f85eb57a31c
ShaikNaziyagit <naziyashaik05l7@gmail.com>
2026-07-11 09:26:00 +0530

    initial

---
+++ public/java_script/content_modules.js
@@ -0,0 +1,10 @@
+modules=
+[{title:"E-mail Phishing",page:"email.html"},
+    {title:"Cyber Bullying",page:"cyberbullying.html"},
+    {title:"Gaming Scams",page:"gaming.html"},
+    {title:"UPI Scams",page:"upi.html"},
+    {title:"Fake E-commerce",page:"fakeecom.html"}
+]
+
+card=document.createElement('div')
+
