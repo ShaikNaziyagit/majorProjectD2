6b949dcd2492ab0a8b3dbae2e0d3f7703ddbb902
Naziya <naziyashaik05l7@gmail.com>
2026-07-20 13:58:46 +0530

    added video cards

---
+++ public/java_script/cyberbullying.js
@@ -0,0 +1,35 @@
+function playVideo(){
+    Element=document.getElementById("content");
+    Element.innerHTML=
+    <div class="lesson">
+        <h2>CYBERBULLYING VIDEO LESSON</h2>
+        <video controls width="600">
+            <source src="" type="video/mp4"/>
+        </video>
+    </div>
+}
+ async function readstories(){
+    element=document.getElementById("content");
+    element.innerHTML=
+    <div class="story">
+        <h2>GENERATING YOUR STORY..............</h2>
+         </div>
+        try{
+            const response=await
+            fetch(
+                "https://localhost:3000/api/generate-story",
+                {method:"POST",
+                headers:{"content-Type":"application/json"},
+                body:JSON.stringify({
+                    topic:"Cyberbullying"
+                })
+
+                }
+
+            );
+        const data=await response.json();
+        
+        }
+   
+
+}
